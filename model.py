2607786953eaf277c9725dd2ff6f9107b1f9e750
flying_matrix <135624430+FlyingMatrix@users.noreply.github.com>
2025-04-27 12:06:30 +0200

    Update model.py

--- model.py
+++ model.py
@@ -48,3 +48,32 @@ class PositionalEncoding(nn.Module):
         return self.dropout(x)
         
 
+class LayerNormalization(nn.Module): # can also use layer_norm = nn.LayerNorm(dim_model)
+
+    def __init__(self, eps=10**-6):
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1)) # multiplied
+        self.bias = nn.Parameter(torch.zeros(1)) # added
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        std = x.std(dim=-1, keepdim=True)
+        layernorm = self.alpha * (x - mean) / (std + self.eps) + self.bias
+        return layernorm
+    
+
+class FeedForwardBlock(nn.Module):
+    
+    def __init__(self, dim_model, dim_ff, dropout):
+        super().__init__()
+        self.linear_1 = nn.Linear(dim_model, dim_ff) # W1 and B1
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(dim_ff, dim_model) # W2 and B2
+
+    def forward(self, x):
+        # (batch_size, seq_len, dim_model) -> (batch_size, seq_len, dim_ff) -> (batch_size, seq_len, dim_model)
+        feedforward = self.linear_2(self.dropout(torch.relu(self.linear_1(x)))) # torch.relu(x) = max(0, x)
+        return feedforward
+    
+

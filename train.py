4e740b08d6f0668ec07f34c62da752d0591335f9
flying_matrix <135624430+FlyingMatrix@users.noreply.github.com>
2025-05-09 00:35:51 +0200

    Add files via upload

---
+++ train.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader, random_split
+
+# Huggingface tokenizers
+"""
+    Huggingface Tokenizer best for: Transformer-based models (BERT, GPT, etc.)
+"""
+import torchtext.datasets as datasets
+from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.trainers import WordLevelTrainer
+from tokenizers.pre_tokenizers import Whitespace
+
+from pathlib import Path
+
+
+

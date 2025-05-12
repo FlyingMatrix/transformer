babee9b9ff2e806ee8f229306d109d97b2144ad4
flying_matrix <135624430+FlyingMatrix@users.noreply.github.com>
2025-05-12 11:43:39 +0200

    Update train.py

--- train.py
+++ train.py
@@ -5,11 +5,13 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader, random_split
 
+import torchtext.datasets as datasets
+from datasets import load_dataset
+
 # Hugging Face tokenizers
 """
     Hugging Face tokenizers best for: Transformer-based models (BERT, GPT, etc.)
 """
-import torchtext.datasets as datasets
 from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
 from tokenizers.trainers import WordLevelTrainer
@@ -60,6 +62,19 @@ def get_tokenizer(config, dataset, language):
 
 def get_dataset(config):
 
-    pass
+    # get dataset
+    dataset = load_dataset(path=f"{config['datasource']}",
+                           name=f"{config['lang_src']}-{config['lang_tar']}",
+                           split='train')
 
+    # build tokenizers
+    tokenizer_src = get_tokenizer(config, dataset, config['lang_src'])
+    tokenizer_tar = get_tokenizer(config, dataset, config['lang_tar'])
+
+    # split 90% of the dataset for training, 10% for validation
+    train_dataset_size = int(0.9 * len(dataset))
+    valid_dataset_size = len(dataset) - train_dataset_size
+    train_dataset, valid_dataset = random_split(dataset, [train_dataset_size, valid_dataset_size])
+        
+    
 

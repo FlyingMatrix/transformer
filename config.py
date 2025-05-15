93bd3637f1876ea5ce30056bd5e0a252bcaa49b3
flying_matrix <135624430+FlyingMatrix@users.noreply.github.com>
2025-05-15 11:33:24 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+
+def get_config():
+    
+    return {
+        "batch_size": 8,
+        "num_epochs": 20,
+        "learning_rate": 10**-4,
+        "seq_len": 350,
+        "dim_model": 512,
+        "data_source": 'opus_books',
+        "language_src": "en",
+        "language_tar": "it"
+    }

d0409b92629d607e4a7dcaa5513b2ed8bdfcc630
flying_matrix <135624430+FlyingMatrix@users.noreply.github.com>
2025-05-19 11:15:04 +0200

    Update config.py

--- config.py
+++ config.py
@@ -14,6 +14,19 @@ def get_config():
         "dim_model": 512,
         "data_source": 'opus_books',
         "lang_src": "en",
-        "lang_tar": "it"
+        "lang_tar": "it",
+        "model_folder": "weights",
+        "model_basename": "transformer_model_",
+        "preload": "latest",
+        "tokenizer_file": "tokenizer_{0}.json",
+        "experiment_name": "runs/transformer_model"
     }
+
+
+def get_weights_file_path(config, epoch: str):
+    
+    model_folder = f"{config['data_source']}_{config['model_folder']}"
+    model_filename = f"{config['model_basename']}{epoch}.pt"
+    return str(Path('.') / model_folder / model_filename) # "./opus_books_weights/transformer_model_epoch.pt"
+
 
